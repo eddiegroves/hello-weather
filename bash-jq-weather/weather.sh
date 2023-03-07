225a82518ead8254328891f52d2696624237acd9
Eddie Groves <git@eddiegroves.com>
2023-03-08 05:16:17 +1100

    bash: Allow script to be called from any directory

--- bash-jq-weather/weather.sh
+++ bash-jq-weather/weather.sh
@@ -1,2 +1,3 @@
+SCRIPTPATH=$(dirname "$(realpath "$0")")
 curl --silent --show-error "$BASE_URL/v2/$WILLYWEATHER_API_KEY/locations/$LOCATION_ID/weather.json?forecasts=weather&observational=true" | \
-    gojq -f weather.jq
+    gojq -f "$SCRIPTPATH/weather.jq"
